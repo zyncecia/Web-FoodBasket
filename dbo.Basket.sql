57d910364c86bcb5860c121c01779ce53ff708b8
zyncecia <100898620+zyncecia@users.noreply.github.com>
2022-03-31 13:53:56 +0900

    Add files via upload

---
+++ dbo.Basket.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Basket] (
+    [ID]        INT            IDENTITY (1, 1) NOT NULL,
+    [FirstName] NVARCHAR (MAX) NULL,
+    [LastName]  NVARCHAR (MAX) NULL,
+    [ContactNo] FLOAT (53)     NOT NULL,
+    [Gender]    NVARCHAR (MAX) NULL,
+    [Address]   NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_Basket] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
